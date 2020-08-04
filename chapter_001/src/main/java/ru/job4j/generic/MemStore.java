bfa2d9c040a4c90dc1adec25545d156649392915
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-08-04 16:47:33 +0700

    5.2.2. Реализовать Store<T extends Base>[157#336893] UPD

--- chapter_001/src/main/java/ru/job4j/generic/MemStore.java
+++ chapter_001/src/main/java/ru/job4j/generic/MemStore.java
@@ -2,6 +2,7 @@ package ru.job4j.generic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class MemStore<T extends Base> implements Store<T> {
 
@@ -15,9 +16,9 @@ public final class MemStore<T extends Base> implements Store<T> {
     @Override
     public boolean replace(String id, T model) {
         boolean ret = false;
-        T lModel = findById(id);
-        if (lModel != null) {
-            mem.set(mem.indexOf(lModel), model);
+        int i = findIndexById(id);
+        if (i != -1) {
+            mem.set(i, model);
             ret = true;
         }
 
@@ -44,5 +45,14 @@ public final class MemStore<T extends Base> implements Store<T> {
             }
         }
         return null;
+    }
+
+    public int findIndexById(String id) {
+        for (int i = 0; i < mem.size(); i++) {
+            if (Objects.equals(mem.get(i).getId(), id)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
