893dbfa1a2b916c0d8be0adae0d81c256996de06
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-08-05 20:07:04 +0700

    5.3.2. Создать контейнер на базе связанного списка [159#336872]

---
+++ chapter_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,74 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+
+public class ForwardLinked<E> implements Iterable<E> {
+    private final float multiplier = 1.5f;
+    private Node<E> head;
+    private Node<E> tail;
+    private int modCount;
+    private int size;
+
+    public int size() {
+        return size;
+    }
+
+    public void add(E value) {
+        size++;
+        modCount++;
+        Node<E> newNode = new Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        int i = 0;
+        Node<E> node = head;
+        while (i < index) {
+            node = node.next;
+            i++;
+        }
+        return node.value;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int pointer;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return pointer < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return get(pointer++);
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ForwardLinkedTest {
+    @Test
+    public void whenAddThenGet() {
+        ForwardLinked<String> list = new ForwardLinked<>();
+        list.add("first");
+        String rsl = list.get(0);
+        assertThat(rsl, is("first"));
+    }
+
+    @Test
+    public void whenAddThenIt() {
+        ForwardLinked<String> list = new ForwardLinked<>();
+        list.add("first");
+        String rsl = list.iterator().next();
+        assertThat(rsl, is("first"));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetEmpty() {
+        ForwardLinked<String> list = new ForwardLinked<>();
+        list.get(0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetOutBound() {
+        ForwardLinked<String> list = new ForwardLinked<>();
+        list.add("first");
+        list.get(1);
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenGetEmptyFromIt() {
+        ForwardLinked<String> list = new ForwardLinked<>();
+        list.iterator().next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenCorruptedIt() {
+        ForwardLinked<String> list = new ForwardLinked<>();
+        list.add("first");
+        Iterator<String> it = list.iterator();
+        list.add("second");
+        it.next();
+    }
+}
