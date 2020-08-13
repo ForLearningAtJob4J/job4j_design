d74adf4aac5365618e5f2d95053bf25fc8f29d94
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-08-13 13:38:54 +0700

    6.1.0.3. BufferedReader. [#336920] UPD

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -8,9 +8,7 @@ import java.util.stream.Collectors;
 public class LogFilter {
     public static List<String> filter(String fileName) {
         try (BufferedReader in = new BufferedReader(new FileReader(fileName))) {
-            List<String> lines = new ArrayList<>();
-            in.lines().forEach(lines::add);
-            return lines.stream()
+            return in.lines()
                     .filter(s -> s.substring(s.lastIndexOf(" ") - 3, s.lastIndexOf(" ")).equals("404"))
                     .collect(Collectors.toList());
         } catch (Exception e) {
