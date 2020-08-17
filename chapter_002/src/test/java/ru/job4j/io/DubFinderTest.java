56d0b836dc45fa2f31623d092f65ae6a4fda4830
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-08-17 16:29:55 +0700

    6.1.4.2. Поиск дубликатов [#336924] 2

--- chapter_002/src/test/java/ru/job4j/io/DubFinderTest.java
+++ chapter_002/src/test/java/ru/job4j/io/DubFinderTest.java
@@ -3,8 +3,7 @@ package ru.job4j.io;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,18 +16,18 @@ public class DubFinderTest {
     public void getCopiesTest() throws IOException {
         DubFinder dirWalker = new DubFinder();
         Files.walkFileTree(Paths.get("../data/test"), dirWalker);
-//        System.out.println(Paths.get("../data/test").toAbsolutePath());
+        System.out.println(Paths.get("../data/test").toAbsolutePath());
         Map<DubFinder.FileInfo, List<DubFinder.FileInfo>> dubsMap = dirWalker.getCopies();
 
-//        dubsMap.forEach((fileInfo, fileInfos) -> {
-//            System.out.println(fileInfo.name + " == " + fileInfo.size + " bytes");
-//            fileInfos.forEach(path -> System.out.println("\t" + path.realPath));
-//        });
+        dubsMap.forEach((fileInfo, fileInfos) -> {
+            System.out.println(fileInfo.name + " == " + fileInfo.size + " bytes");
+            fileInfos.forEach(path -> System.out.println("\t" + path.realPath));
+        });
         assertThat(dubsMap.size(), is(2));
         DubFinder.FileInfo fi1 = new DubFinder.FileInfo("res.txt", 40);
         List<DubFinder.FileInfo> element1 = dubsMap.get(fi1);
         System.out.println(element1);
-        assertNull(null);
+        assertNotNull(element1);
 //        assertThat(dubsMap.get(fi1).size(), is(3));
 //        DubFinder.FileInfo fi2 = new DubFinder.FileInfo("status.txt", 82);
 //        assertThat(dubsMap.get(fi2), notNullValue());
