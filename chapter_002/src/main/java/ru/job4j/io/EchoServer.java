f22d42540f2ba40eac491feb116a41cf1de922a2
Sergey Sapunov <youcanwriteme@yandex.ru>
2020-08-18 12:57:23 +0700

    6.2.1. Бот [#336942]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -8,48 +8,72 @@ import java.util.stream.Collectors;
 
 public class EchoServer {
     final static String QUIT_MESSAGE = "BYE";
+    final static String HELLO_MESSAGE = "HELLO";
+
     final static String REQUEST_METHOD = "GET";
 
     static List<String> request = new ArrayList<>();
+    static final int PORT = 9000;
 
-    @SuppressWarnings("checkstyle:InnerAssignment")
     public static void main(String[] args) throws IOException {
         boolean needToWork = true;
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (needToWork) {
-                Socket socket = server.accept();
-                System.out.println("Query received");
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    String str = in.readLine();
-                    while (!str.isEmpty()) {
-                        System.out.println(str);
-                        request.add(str);
-                        str = in.readLine();
-                    }
-                    if (QUIT_MESSAGE.equals(parseCommand(request).toUpperCase())) {
-                        System.out.println("Exiting server process...");
-                        needToWork = false;
-                    }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+        System.out.println("Server started. Listening on port " + PORT + "...");
+        ServerSocket server = new ServerSocket(PORT);
+        while (needToWork) {
+            Socket socket = server.accept();
+            System.out.println("Query received");
+            try (OutputStream out = socket.getOutputStream();
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                String str = in.readLine();
+                while (str != null && !str.isEmpty()) {
+                    request.add(str);
+                    str = in.readLine();
                 }
+                String command = parseCommand(request);
+                if (QUIT_MESSAGE.equals(command.toUpperCase())) {
+                    needToWork = false;
+                }
+
+                String content;
+
+                if (HELLO_MESSAGE.equals(command.toUpperCase())) {
+                    content = "Hello, my dear friend!";
+                } else if (QUIT_MESSAGE.equals(command.toUpperCase())) {
+                    content = "Exiting server process...";
+                } else {
+                    content = command;
+                }
+
+                System.out.println("Sending: " + content);
+                out.write("HTTP/1.1 200 OK\r\n".getBytes());
+
+                out.write(("Content-Length: " + content.length() + "\r\n").getBytes());
+                out.write("Content-Type: text/html\r\n".getBytes());
+                out.write("Connection: Close\r\n\r\n".getBytes());
+
+                out.write(content.getBytes());
             }
         }
+        System.out.println("Server process finished!");
     }
 
     private static String parseCommand(List<String> request) {
         String buf = request.get(0);
         if (buf != null) {
             buf = buf.trim();
-            if (REQUEST_METHOD.equals(buf.substring(0, buf.indexOf(" ")))) {
-                buf = buf.substring(buf.indexOf("?") + 1, buf.lastIndexOf(" "));
-                Map<String, String> query = Arrays.stream(buf.split("&"))
-                        .collect(Collectors.toMap(
-                                s -> s.substring(0, s.indexOf("=")).toUpperCase(),
-                                s -> s.substring(s.indexOf("=") + 1))
-                        );
-                buf = query.get("MSG");
-                System.out.println(buf);
+            System.out.println(buf);
+            try {
+                if (REQUEST_METHOD.equals(buf.substring(0, buf.indexOf(" ")))) {
+                    buf = buf.substring(buf.indexOf("?") + 1, buf.lastIndexOf(" "));
+                    Map<String, String> query = Arrays.stream(buf.split("&"))
+                            .collect(Collectors.toMap(
+                                    s -> s.substring(0, s.indexOf("=")).toUpperCase(),
+                                    s -> s.substring(s.indexOf("=") + 1))
+                            );
+                    buf = query.get("MSG");
+                }
+            } catch (Exception e) {
+                buf = e.getMessage();
             }
         }
         request.clear();
