93f5080aef7a554930a30d645cffc99d5f8991c8
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-01-02 23:51:18 +0700

    8.4.5.1. Экзамен. GC. [#336984] upd

--- chapter_004/src/main/java/ru/job4j/task/Article.java
+++ chapter_004/src/main/java/ru/job4j/task/Article.java
@@ -6,14 +6,14 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 1, time = 2)
+@Warmup(iterations = 2, time = 2)
 @Measurement(iterations = 3, time = 2)
 public class Article {
     static String text = "Мой дядя самых честных правил, "
@@ -32,11 +32,6 @@ public class Article {
             + "Когда же черт возьмет тебя!";
     static String check = "Мой дядя мог думать про тебя и день и ночь";
 
-    @Benchmark
-    public void init() {
-        generateBy(text, check);
-    }
-
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(Article.class.getSimpleName())
@@ -44,15 +39,15 @@ public class Article {
                 .build();
 
         new Runner(opt).run();
-//        System.out.println(generateBy(text, check));
     }
 
-    public static boolean generateBy(String origin, String line) {
-        var source = Arrays.stream(origin.replaceAll("[^a-zA-Zа-яёА-ЯЁ ]", "").split(" ")).collect(Collectors.groupingBy(v -> v));
-        var test = Arrays.stream(line.replaceAll("[^a-zA-Zа-яёА-ЯЁ ]", "").split(" ")).collect(Collectors.groupingBy(v -> v));
+    @Benchmark
+    public static boolean generateByv1() {
+        var source = Arrays.stream(text.replaceAll("[^a-zA-Zа-яёА-ЯЁ ]", "").split(" ")).collect(Collectors.groupingBy(v -> v));
+        var test = Arrays.stream(check.replaceAll("[^a-zA-Zа-яёА-ЯЁ ]", "").split(" ")).collect(Collectors.groupingBy(v -> v));
 
         boolean can = true;
-        for (String key: test.keySet()) {
+        for (String key : test.keySet()) {
             if (!source.containsKey(key)
                     || source.get(key).size() < test.get(key).size()) {
                 can = false;
@@ -62,4 +57,14 @@ public class Article {
         return can;
     }
 
+    @Benchmark
+    public static boolean generateByv2() {
+        String[] source = text.replaceAll("[^a-zA-Zа-яёА-ЯЁ ]", "").split(" ");
+        String[] test = check.replaceAll("[^a-zA-Zа-яёА-ЯЁ ]", "").split(" ");
+
+        List<String> testList = new ArrayList<>(Arrays.asList(test));
+        testList.removeAll(Arrays.asList(source));
+
+        return testList.size() == 0;
+    }
 }
