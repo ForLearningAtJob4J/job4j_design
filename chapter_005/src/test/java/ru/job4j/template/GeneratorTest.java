e486748bf74df3d7b0daf7c6d661a4d26f0ae140
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-01-05 05:12:49 +0700

    8.5.0.3. Шаблонизатор. [#336946]

---
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+
+    @Test
+    public void produce() {
+        Generator generator = new GeneratorString();
+        assertEquals("I am Vanya", generator.produce("I am ${name}", Map.of("name", "Vanya")));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceBadTemplate() {
+        Generator generator = new GeneratorString();
+        generator.produce("I am ${name} from ${city}", Map.of("name", "Vanya"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceBadMap() {
+        Generator generator = new GeneratorString();
+        generator.produce("I am ${name}", Map.of("name", "Vanya", "age", "111"));
+    }
+}
