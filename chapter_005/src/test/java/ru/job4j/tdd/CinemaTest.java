353f1aa23245eb992ab1242a5707408d3d088ee8
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-01-06 04:30:05 +0700

    8.5.0.2. Что такое TDD? [#336947] upd

--- chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
+++ chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,13 +1,11 @@
 package ru.job4j.tdd;
 
 import static org.junit.Assert.assertEquals;
-import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.List;
+import java.util.*;
 
 public class CinemaTest {
 
@@ -18,7 +16,18 @@ public class CinemaTest {
         Calendar date = Calendar.getInstance();
         date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
-//        assertEquals(ticket, new Ticket3D());
+        assertEquals(ticket, new Ticket3D());
+    }
+
+    @Test
+    public void buyBusied() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.NOVEMBER, 10, 23, 0);
+        cinema.buy(account, 1, 1, date);
+        Ticket nullTicket = cinema.buy(account, 1, 1, date);
+        assertNull(nullTicket);
     }
 
     @Test
@@ -26,7 +35,15 @@ public class CinemaTest {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
-//        assertEquals(sessions, Arrays.asList(new Session3D()));
+        assertEquals(sessions, Collections.singletonList(new Session3D()));
+    }
+
+    @Test
+    public void notFound() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertEquals(sessions, Collections.emptyList());
     }
 
     @Test
@@ -34,6 +51,6 @@ public class CinemaTest {
         Cinema cinema = new Cinema3D();
         Session session = new Session3D();
         cinema.add(session);
-//        assertEquals(session, cinema.find(ses -> true).get(0));
+        assertEquals(session, cinema.find(ses -> true).get(0));
     }
 }
