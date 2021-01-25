4f8047f7e8b4816f084568797b6dc79408187f6e
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-01-26 00:54:41 +0700

    6.2.4.3. Формат XML [#336933]

---
+++ chapter_002/src/main/java/ru/job4j/serialization/xml/Computer.java
@@ -0,0 +1,11 @@
+package ru.job4j.serialization.xml;
+
+import ru.job4j.serialization.json.Person;
+
+public class Computer {
+    boolean onBalance;
+    int value;
+    String model;
+    Person owner;
+    Person[] previousOwners;
+}
